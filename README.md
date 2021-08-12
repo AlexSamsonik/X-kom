5d32ade4ba74848a99cc1d709744b2b91353992a
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2021-08-12 13:17:01 +0300

    Run parameters have been added.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # X-kom
 Get detailed information about item in internet-market x-kom.pl
+
+**Polish recourse:** https://www.x-kom.pl/
+
+How to use:
+ - clone the repository;
+ - install and activate virtualenv if necessary;
+ - install requirement `(venv) $ pip install -r requirement.txt`
+ - run main.py `(venv) $ python3 main.py -k x-kom-kod`
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """There is main function."""
+from argparse import ArgumentParser
+
 from api.xkom_client import XKomClient
 
 if __name__ == '__main__':
-    x_kom_kod = 540711  # TODO: Create configuration for running.
-    x_kom_response = XKomClient().get_request(x_kom_kod)
+    parser = ArgumentParser('Parameters for getting detailed information about item in internet-market x-kom.pl')
+    parser.add_argument('-k', '--kod', help='X-Kom kod', required=True)
+    args = parser.parse_args()
+
+    x_kom_response = XKomClient().get_request(args.kod)
     print(f"{x_kom_response.producer.name} {x_kom_response.name} - {x_kom_response.price} zł")
